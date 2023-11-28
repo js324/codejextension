a26a0092bad04921a9a1d654773d458f5c615b9c
ZiJun Weng <zjw27@atsil.com>
2023-11-28 16:09:42 -0500

    Basic Popup is established

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -7,7 +7,19 @@ const Popup = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>highl<span style={{ color: "#E63946" }}>A</span><span style={{ color: "#1D3557" }}>I</span>ter</h2>
+      </header>
+      <body className="App-body">
+        <h1>Oscar
+          ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</h1>
+      </body>
+    </div>
+  );
+};
+
+export default Popup;
+
+{/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
         </p>
@@ -18,10 +30,5 @@ const Popup = () => {
           rel="noopener noreferrer"
         >
           Learn React!
-        </a>
-      </header>
-    </div>
-  );
-};
 
-export default Popup;
+        </a> */}
